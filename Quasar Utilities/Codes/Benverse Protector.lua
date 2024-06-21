fddd5efeb4dbe57ed309fd4d371ac6905ac0f98d
krypton <71219830+KrypDeveloper@users.noreply.github.com>
2024-06-21 10:48:17 -0300

    Create Benverse Protector.lua

---
+++ Quasar Utilities/Codes/Benverse Protector.lua
@@ -0,0 +1,15 @@
+return {
+  "SORRYFORSHUTDOWN",
+  "19KLIKES",
+  "9MVISITS",
+  "RAIDSUPDATE",
+  "NEWSALIENS",
+  "UPDATE1",
+  "BinLindo",
+  "NewAliens",
+  "Update",
+  "BugsFixed",
+  "SORRYFORDELAY2",
+  "SORRYFORDELAY",
+  "RELEASE"
+}
