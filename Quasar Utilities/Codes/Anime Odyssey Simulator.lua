0304df2beb3d29608e1565a65941ad238f10c8dc
krypton <71219830+KrypDeveloper@users.noreply.github.com>
2024-06-21 10:50:44 -0300

    Create Anime Odyssey Simulator.lua

---
+++ Quasar Utilities/Codes/Anime Odyssey Simulator.lua
@@ -0,0 +1,15 @@
+return {
+  "SHREDDER",
+  "release",
+  "SHINYBUFF",
+  "SORRYFORSHUTDOWN",
+  "aya",
+  "herootw",
+  "default",
+  "urubuzin",
+  "jeke",
+  "hiro",
+  "playzin",
+  "SORRYFORSHUTDOWN2",
+  "QUESTS"
+}
