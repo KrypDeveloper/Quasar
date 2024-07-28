c806f3d254d09cb7b7eaedf01b56826a06db7e3f
RIOT <71219830+KrypDeveloper@users.noreply.github.com>
2024-07-28 13:21:45 -0300

    Create manager.lua

---
+++ src/manager.lua
@@ -0,0 +1,7 @@
+if setclipboard then
+   setclipboard("https://pastebin.com/YsXwbRU4")
+   task.wait(0.79)
+   game.Players.LocalPlayer:Kick("https://pastebin.com/YsXwbRU4 go to your clipboard")
+else
+   game.Players.LocalPlayer:Kick("Please get the new one from https://pastebin.com/YsXwbRU4")
+end
